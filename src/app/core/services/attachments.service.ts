31138d500c700cf3ef0bc812d97e025f32a4f1df
Álvaro Vaquero Giménez <alvaro.vaquero.tel@gmail.com>
2023-03-30 18:32:53 +0200

    Agregamos service de attachments

---
+++ src/app/core/services/attachments.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AttachmentModel } from '@core/models/attachment.model';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AttachmentsService {
+
+  constructor(
+    private readonly http: HttpClient
+  ) { }
+
+  getAttachment (id: string): Observable<AttachmentModel> {
+    return this.http.get<AttachmentModel>(`${environment.api}/attachments/${id}`);
+  }
+
+  find (): Observable<AttachmentModel[]> {
+    return this.http.get<AttachmentModel[]>(`${environment.api}/attachments/findAll`);
+  }
+
+  create (title: string, file: FormData): Observable<AttachmentModel> {
+    return this.http.post<AttachmentModel>(`${environment.api}/attachments/create/${title}`, file);
+  }
+
+  update (id: string, title: string): Observable<AttachmentModel> {
+    return this.http.patch<AttachmentModel>(`${environment.api}/attachments/update/${id}/${title}`, null);
+  }
+
+  remove (id: string): Observable<AttachmentModel> {
+    return this.http.delete<AttachmentModel>(`${environment.api}/attachments/remove/${id}`);
+  }
+
+}

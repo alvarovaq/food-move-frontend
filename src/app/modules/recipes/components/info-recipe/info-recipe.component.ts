44ee6b4f3dcdbf07849e4b57053b88755860bed6
Álvaro Vaquero Giménez <alvaro.vaquero.tel@gmail.com>
2023-04-03 14:52:37 +0200

    Visualizacion de adjuntos en infos

--- src/app/modules/recipes/components/info-recipe/info-recipe.component.ts
+++ src/app/modules/recipes/components/info-recipe/info-recipe.component.ts
@@ -1,6 +1,11 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { AttachmentModel } from '@core/models/attachment.model';
 import { RecipeModel } from '@core/models/recipe.model';
+import { AttachmentsService } from '@core/services/attachments.service';
+import { LoaderService } from '@core/services/loader.service';
+import { finalize } from 'rxjs';
+import { URL_ATTACHMENTS } from 'src/app/constants/app.constants';
 
 @Component({
   selector: 'app-info-recipe',
@@ -9,12 +14,33 @@ import { RecipeModel } from '@core/models/recipe.model';
 })
 export class InfoRecipeComponent implements OnInit {
 
+  attachment: AttachmentModel | null = null;
+
   constructor(
     private readonly dialogRef: MatDialogRef<InfoRecipeComponent>,
-    @Inject(MAT_DIALOG_DATA) public readonly recipe: RecipeModel
+    @Inject(MAT_DIALOG_DATA) public readonly recipe: RecipeModel,
+    private readonly attachmentsService: AttachmentsService,
+    private readonly loaderService: LoaderService
   ) { }
 
   ngOnInit(): void {
+    if (this.recipe.attachment) {
+      this.loaderService.isLoading.next(true);
+      this.attachmentsService.getAttachment(this.recipe.attachment)
+      .pipe(finalize(() => this.loaderService.isLoading.next(false)))
+      .subscribe(
+        res => {
+          this.attachment = res;
+        },
+        err => {
+          console.log(err);
+        }
+      );
+    }
+  }
+
+  getAttachmentUrl (): string {
+    return URL_ATTACHMENTS + this.attachment?.filename;
   }
 
 }
--- src/app/modules/routines/components/info-routine/info-routine.component.ts
+++ src/app/modules/routines/components/info-routine/info-routine.component.ts
@@ -1,6 +1,11 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { AttachmentModel } from '@core/models/attachment.model';
 import { RoutineModel } from '@core/models/routine.model';
+import { AttachmentsService } from '@core/services/attachments.service';
+import { LoaderService } from '@core/services/loader.service';
+import { finalize } from 'rxjs';
+import { URL_ATTACHMENTS } from 'src/app/constants/app.constants';
 
 @Component({
   selector: 'app-info-routine',
@@ -9,12 +14,33 @@ import { RoutineModel } from '@core/models/routine.model';
 })
 export class InfoRoutineComponent implements OnInit {
 
+  attachment: AttachmentModel | null = null;
+
   constructor(
     private readonly dialogRef: MatDialogRef<InfoRoutineComponent>,
-    @Inject(MAT_DIALOG_DATA) public readonly routine: RoutineModel
+    @Inject(MAT_DIALOG_DATA) public readonly routine: RoutineModel,
+    private readonly attachmentsService: AttachmentsService,
+    private readonly loaderService: LoaderService
   ) {}
 
   ngOnInit(): void {
+    if (this.routine.attachment) {
+      this.loaderService.isLoading.next(true);
+      this.attachmentsService.getAttachment(this.routine.attachment)
+      .pipe(finalize(() => this.loaderService.isLoading.next(false)))
+      .subscribe(
+        res => {
+          this.attachment = res;
+        },
+        err => {
+          console.log(err);
+        }
+      );
+    }
+  }
+
+  getAttachmentUrl (): string {
+    return URL_ATTACHMENTS + this.attachment?.filename;
   }
 
 }

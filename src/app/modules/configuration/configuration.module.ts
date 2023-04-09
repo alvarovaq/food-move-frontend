d2bf66133e2550bdea609189209f1e71058d5ef5
Álvaro Vaquero Giménez <alvaro.vaquero.tel@gmail.com>
2023-04-09 18:00:55 +0200

    Creamos 'ConfigurationPage'

--- src/app/modules/configuration/configuration.module.ts
+++ src/app/modules/configuration/configuration.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { ConfigurationRoutingModule } from './configuration-routing.module';
 import { ConfigurationComponent } from '../configuration/configuration.component';
+import { ConfigurationPageComponent } from './configuration-page/configuration-page.component';
 
 
 @NgModule({
   declarations: [
-    ConfigurationComponent
+    ConfigurationComponent,
+    ConfigurationPageComponent
   ],
   imports: [
     CommonModule,

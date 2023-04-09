fec1d43f04e4e02991cd53c3ec7b75339c337832
Álvaro Vaquero Giménez <alvaro.vaquero.tel@gmail.com>
2023-04-09 18:03:50 +0200

    Agregamos 'ConfigurationPage' en el routing

--- src/app/modules/configuration/configuration-routing.module.ts
+++ src/app/modules/configuration/configuration-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConfigurationComponent } from './configuration.component';
+import { ConfigurationPageComponent } from './configuration-page/configuration-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ConfigurationComponent, children: [
+    { path: '', component: ConfigurationPageComponent},
+    { path: '**', redirectTo: ''}
+  ] }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],

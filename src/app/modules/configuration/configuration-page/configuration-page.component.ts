1994b6736dcd3a88e1476326ba0a6915ac26c7bd
Álvaro Vaquero Giménez <alvaro.vaquero.tel@gmail.com>
2023-04-11 16:57:08 +0200

    Edicion del ConfigurationPage

--- src/app/modules/configuration/configuration-page/configuration-page.component.ts
+++ src/app/modules/configuration/configuration-page/configuration-page.component.ts
@@ -16,27 +16,10 @@ import { finalize } from 'rxjs';
 })
 export class ConfigurationPageComponent implements OnInit {
 
-  form!: FormGroup;
   employee: EmployeeModel | null = null;
-  
-  buttonClear = {
-    name: false,
-    surname: false,
-    email: false,
-    phone: false
-  }
-
-  imageFile?: string = "";
-  selectedFile?: File;
-  removeProfileImage: boolean = false;
 
   constructor(
-    private readonly authService: AuthService,
-    private readonly employeesService: EmployeesService,
-    private readonly fb: FormBuilder,
-    private readonly loaderService: LoaderService,
-    private readonly snackerService: SnackerService,
-    private readonly optionalPipe: OptionalPipe
+    private readonly authService: AuthService
   ) { }
 
   ngOnInit(): void {
@@ -44,7 +27,6 @@ export class ConfigurationPageComponent implements OnInit {
     .subscribe(
       res => {
         this.employee = res;
-        this.initForm();
       },
       err => {
         console.log(err);
@@ -52,116 +34,8 @@ export class ConfigurationPageComponent implements OnInit {
     );
   }
 
-  initForm(): void {
-    this.form = this.fb.group({
-      name: [this.employee!.name, [Validators.required]],
-      surname: [this.employee!.surname],
-      email: [this.employee!.email, [Validators.required, Validators.email]],
-      phone: [this.employee!.phone],
-    });
-  }
-
-  get name (): string | null {
-    return this.form.value.name;
-  }
-
-  get surname (): string | null {
-    return this.form.value.surname;
-  }
-
-  get email (): string | null {
-    return this.form.value.email;
-  }
-
-  get phone (): string | null {
-    return this.form.value.phone;
-  }
-
-  clearField (field: string): void {
-    this.form.value[field] = null;
-    this.form.reset(this.form.value);
-  }
-
-  onSelectFile (event: any): void {
-    this.selectedFile = <File>event.target.files[0];
-    const reader = new FileReader();
-    reader.onload = e => this.imageFile = reader.result as string;
-    reader.readAsDataURL(this.selectedFile);
-  }
-
-  onRemoveProfileImage (): void {
-    this.removeProfileImage = true;
-    this.selectedFile = undefined;
-    this.imageFile = "";
-  }
-
-  onRecoverProfileImage (): void {
-    this.removeProfileImage = false;
-    this.selectedFile = undefined;
-    this.imageFile = "";
-  }
-
-  resetProfile (): void {
-    this.initForm();
-  }
-
-  editProfile (): void {
-    this.loaderService.isLoading.next(true);
-    const employee = this.getEmployeeRequest(true);
-    this.employeesService.updateEmployee(this.employee!._id, employee)
-    .pipe(finalize(() => {
-      this.loaderService.isLoading.next(false);
-    }))
-    .subscribe(
-      res => {
-        this.employee = res;
-        if (this.selectedFile) {
-          const fd = new FormData();
-          fd.append('file', this.selectedFile!, this.selectedFile?.name);
-          this.employeesService.uploadProfileImage(res._id, fd)
-          .subscribe(
-            res => {
-              this.snackerService.showSuccessful("Perfil editado con éxito");
-              this.authService.refreshUser();
-            },
-            err => {
-              console.log(err);
-              this.snackerService.showError("Error al subir la foto la foto de perfil")
-            }
-          )
-        } else if (this.removeProfileImage) {
-          this.employeesService.removeProfileImage(res._id)
-          .subscribe(
-            res => {
-              this.snackerService.showSuccessful("Perfil editado con éxito");
-              this.authService.refreshUser();
-            },
-            err => {
-              console.log(err);
-              this.snackerService.showError("Error al eliminar la foto de perfil");
-            }
-          )
-        } else {
-          this.snackerService.showSuccessful("Perfil editado con éxito");
-          this.authService.refreshUser();
-        }
-      },
-      err => {
-        console.log(err);
-        this.snackerService.showError(err.error.message);
-      }
-    );
-  }
-
-  private getEmployeeRequest (edit: boolean = false): EmployeeRequestModel {
-    const request = {
-      name: this.name,
-      surname: this.surname,
-      email: this.email,
-      password: '123456789',
-      phone: this.phone
-    };
-    return edit ? request : this.optionalPipe.transform(request);
+  setEmployee (employee: EmployeeModel | null): void {
+    this.employee = employee;
   }
 
 }

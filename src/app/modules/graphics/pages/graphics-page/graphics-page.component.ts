413e694ab84016e6da89556d23afab3e340bc8a0
Álvaro Vaquero Giménez <alvaro.vaquero.tel@gmail.com>
2023-04-18 15:39:48 +0200

    Instalacion luxon

--- src/app/modules/graphics/pages/graphics-page/graphics-page.component.ts
+++ src/app/modules/graphics/pages/graphics-page/graphics-page.component.ts
@@ -3,12 +3,13 @@ import { ActivatedRoute } from '@angular/router';
 import { PatientsService } from '../../../../core/services/patients.service';
 import { PatientModel } from '../../../../core/models/patient.model';
 import { LoaderService } from '../../../../core/services/loader.service';
-import { finalize } from 'rxjs/operators';
 import { RouterService } from '../../../../core/services/router.service';
 import { SnackerService } from '@core/services/snacker.service';
 import { ViewPatientService } from '../../../../core/services/view-patient.service';
 import { BaseChartDirective } from 'ng2-charts';
 import { ChartConfiguration, ChartEvent, ChartType } from 'chart.js';
+import 'chartjs-adapter-luxon';
+import { DateTime } from 'luxon';
 
 @Component({
   selector: 'app-graphics-page',
@@ -42,10 +43,18 @@ export class GraphicsPageComponent implements OnInit {
     );
   }
 
-  public lineChartData: ChartConfiguration['data'] = {
+  newDate (days: number): Date {
+    return DateTime.now().plus({days}).toJSDate();
+  }
+
+  newDateString (days: number): string | null {
+    return DateTime.now().plus({days}).toISO();
+  }
+
+  public lineChartData = {
     datasets: [
       {
-        data: [ 65, 59, 80, 81, 56, 55, 40 ],
+        data: [ {x: this.newDateString(1), y: 120}, {x: this.newDateString(3), y: 100}, {x: this.newDateString(6), y: 115}, {x: this.newDateString(7), y: 119}],
         label: 'Series A',
         backgroundColor: 'rgba(148,159,177,0.2)',
         borderColor: 'rgba(148,159,177,1)',
@@ -54,32 +63,8 @@ export class GraphicsPageComponent implements OnInit {
         pointHoverBackgroundColor: '#fff',
         pointHoverBorderColor: 'rgba(148,159,177,0.8)',
         fill: 'origin',
-      },
-      {
-        data: [ 28, 48, 40, 19, 86, 27, 90 ],
-        label: 'Series B',
-        backgroundColor: 'rgba(77,83,96,0.2)',
-        borderColor: 'rgba(77,83,96,1)',
-        pointBackgroundColor: 'rgba(77,83,96,1)',
-        pointBorderColor: '#fff',
-        pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: 'rgba(77,83,96,1)',
-        fill: 'origin',
-      },
-      {
-        data: [ 180, 480, 770, 90, 1000, 270, 400 ],
-        label: 'Series C',
-        yAxisID: 'y1',
-        backgroundColor: 'rgba(255,0,0,0.3)',
-        borderColor: 'red',
-        pointBackgroundColor: 'rgba(148,159,177,1)',
-        pointBorderColor: '#fff',
-        pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: 'rgba(148,159,177,0.8)',
-        fill: 'origin',
       }
-    ],
-    labels: [ 'January', 'February', 'March', 'April', 'May', 'June', 'July' ]
+    ]
   };
 
   public lineChartOptions: ChartConfiguration['options'] = {
@@ -89,20 +74,16 @@ export class GraphicsPageComponent implements OnInit {
       }
     },
     scales: {
-      // We use this empty structure as a placeholder for dynamic theming.
-      y:
-        {
-          position: 'left',
-        },
-      y1: {
-        position: 'right',
-        grid: {
-          color: 'rgba(255,0,0,0.3)',
-        },
-        ticks: {
-          color: 'red'
+      x: {
+        type: 'time',
+        time: {
+          tooltipFormat: 'DD T',
+          unit: 'day'
         }
-      }
+      },
+      y: {
+        position: 'left',
+      },
     },
 
     plugins: {
